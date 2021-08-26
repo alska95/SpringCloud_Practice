eabf08a639e6c0f4d6e5703653204203ab9a7559
alska95 <rhsalska55@naver.com>
2021-08-26 23:48:42 +0900

    catalogMicroService - 구현

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into catalog(product_id, product_name , stock, unit_price)
+    values('CATALOG1' , 'pen', 100, 1000);
+insert into catalog(product_id, product_name , stock, unit_price)
+    values('CATALOG2' , 'pen', 10, 1000);
+insert into catalog(product_id, product_name , stock, unit_price)
+    values('CATALOG3' , 'pen', 1, 1000);
+insert into catalog(product_id, product_name , stock, unit_price)
+    values('CATALOG4' , 'pen', 0, 1000);
