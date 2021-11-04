dc910327501af3fd21335f61ad91eaf9de089248
alska95 <rhsalska55@naver.com>
2021-11-05 07:10:50 +0900

    catalogMicroService - Kafka적용 orderService에서 produce된 메시지 받아서 db업데이트함.(consumer) order정보의 productId를 가져옴 -> order의 quantity만큼 수량 제거

---
+++ catalog-service/src/main/java/com/example/catalogservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,43 @@
+package com.example.catalogservice.messagequeue;
+
+import com.example.catalogservice.domain.CatalogEntity;
+import com.example.catalogservice.repository.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+    final CatalogRepository catalogRepository;
+
+    public KafkaConsumer(CatalogRepository catalogRepository) {
+        this.catalogRepository = catalogRepository;
+    }
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQuantity(String kafkaMessage){
+        log.info("kafaka Message: --> "+ kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            });
+        }catch (JsonProcessingException ex){
+            ex.printStackTrace();;
+        }
+        CatalogEntity targetProduct = catalogRepository.findByProductId((String) map.get("productId"));
+        if(targetProduct != null){
+            targetProduct.setStock(targetProduct.getStock() - (Integer)map.get("qty"));
+        }
+    }
+
+
+}
