37da0ba518d2ef66e9a2f54f217f09ca1f1d2ea4
alska95 <rhsalska55@naver.com>
2024-01-12 19:28:40 +0900

    Revert "Revert "[CircuitBreaker] userService에서 orderService호출할 때 적용""
    This reverts commit 7f6fe592d4d31eeed95366dcb3a193158ca6f3be.

--- userservice/src/main/java/com/example/userservice/repository/UserRepository.java
+++ userservice/src/main/java/com/example/userservice/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.example.userservice.repository;
 
 import com.example.userservice.domain.UserEntity;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,9 +8,9 @@ import java.util.Optional;
 
 public interface UserRepository  {
     void save(UserEntity userEntity);
-    Optional<UserEntity> find(String userId);
+    Optional<UserEntity> findByUserId(String userId);
     List<UserEntity> findAll();
     Optional<UserEntity> findByEmail(String email);
-    //crudRepository 사용할 경우 , find를 사용할 경우 select를 추가, 그다음 by 대상 칼럼
+    //crudRepository 사용해도 되지만 그냥 놔둠
 
 }
