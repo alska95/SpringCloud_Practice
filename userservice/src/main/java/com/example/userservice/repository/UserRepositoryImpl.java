fb281c55865eca50b0ff619e7cc08b02ecdb5ffa
alska95 <rhsalska55@naver.com>
2024-01-17 01:47:49 +0900

    [UserService] getResultStream 오류 수정 jpa에서 stream을 사용할 때 try-with-resources로 감싸주지 않으면 stream이 닫히지 않은 경우 db커넥션도 닫히지 않기 때문에 예외 처리를 해주어야 한다. 이는 stream을 사용하면 디폴트로 lazy loading방식으로 데이터를 로드하기 때문인데 필요할 때 데이터를 조회해야 하기 때문에 db커넥션을 닫을 수 없는 것이다. actuator endpoint 오타 수정

--- userservice/src/main/java/com/example/userservice/repository/UserRepositoryImpl.java
+++ userservice/src/main/java/com/example/userservice/repository/UserRepositoryImpl.java
@@ -23,7 +23,7 @@ public class UserRepositoryImpl implements UserRepository {
     public Optional<UserEntity> findByUserId(String userId) {
         return entityManager.createQuery("select u from UserEntity u where u.userId =: userId" , UserEntity.class)
                 .setParameter("userId" , userId)
-                .getResultStream().findFirst();
+                .getResultList().stream().findFirst();
     }
 
     @Override
@@ -35,6 +35,11 @@ public class UserRepositoryImpl implements UserRepository {
     public Optional<UserEntity> findByEmail(String email) {
         return entityManager.createQuery("select u from UserEntity u where u.email =: email" , UserEntity.class)
                 .setParameter("email" , email)
-                .getResultStream().findFirst();
+                .getResultList().stream().findFirst();
     }
+    /**
+     * jpa에서 stream을 사용할 때 try-with-resources로 감싸주지 않으면 stream이 닫히지 않은 경우 db커넥션도 닫히지 않기 때문에 예외 처리를 해주어야 한다.
+     * 이는 stream을 사용하면 디폴트로 lazy loading방식으로 데이터를 로드하기 때문이다.
+     * stream의 데이터를 사용 할 때 조회해야 하기 때문에 stream이 닫히기 전까진 db커넥션을 닫을 수 없는 것이다.
+     * */
 }
